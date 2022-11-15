acfa07226a62bac5525c75020f8bbe131c4db398
rk121 <roshankhadka613@gmail.com>
2022-11-15 21:22:14 +1100

    commit tree e2fe22a9db576f6c107b6a6ff3760cdfc8231023

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+require "pry"
 
+class Book
+    attr_reader :title
+    attr_accessor :page_count, :author, :genre
 
+    def initialize(title, author="Agatha Christie", page_count=272, genre='Mystery')
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+binding.pry
